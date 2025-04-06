8beecdb8265a03cf0640dead87dfcc2a11cf92a5
Siddhant Sahoo <siddhant.kumarsahoo2402@gmail.com>
2025-04-06 20:14:38 +0530

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AuthForm from '@/components/AuthForm'
+const page = () => {
+  return (
+    <div className='auth-layout'>
+      <AuthForm type='sign-in' />
+    </div>
+  )
+}
+
+export default page
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AuthForm from '@/components/AuthForm'
+const page = () => {
+  return (
+    <div className='auth-layout'>
+     <AuthForm type='sign-up'></AuthForm>
+    </div>
+  )
+}
+
+export default page
